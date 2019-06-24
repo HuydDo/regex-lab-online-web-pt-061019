68d7c4e9b8ecc702f99159735fbe8d655df596ec
Huy Do <huyddo40@gmail.com>
2019-06-24 18:47:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,11 +5,11 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w+ing/)
-  binding.pry
+  # binding.pry
 end
 
 def words_five_letters_long(text)
-  text.grep(/^\w{5}/)
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
